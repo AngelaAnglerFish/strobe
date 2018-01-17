e2627f523ad5e876b303bc4a83b6754bb033066f
andre-geldenhuis <andre.geldenhuis@vuw.ac.nz>
2018-01-17 16:44:10 +1300

    Test2, still weird, think it's timer interrupts

--- src/main.cpp
+++ src/main.cpp
@@ -1,85 +1,87 @@
 #include "Arduino.h"
-#include <TimerOne.h>
+#include "TimerOne.h"
 
-//some code from https://playground.arduino.cc/Main/Readingrps
-//strobe modified from TimerOne examples
-
-//rps reading settingsS
-//read rps and calculate average every second
 const int numreadings = 5;
 volatile unsigned long readings[numreadings];
+volatile int index = 0;
 unsigned long average = 0;
 unsigned long period1 = 0;
-volatile int index = 0;
-unsigned long total;
+unsigned long total=0;
 
-volatile bool strobing = false;
+volatile unsigned block_timer = 0;
+volatile bool block=false;
 
-int rpscount = 0;
-unsigned long rps = 0;
-unsigned long lastmillis = 0;
+volatile bool calcnow = false;
 
 //Strobe settings
 const int strobePin = 9;
 float dutyCycle = 5.0;  //percent
 float dutyCycle_use = (dutyCycle / 100) * 1023;
-float strobe_period_float = 10000000;
-unsigned long period_microsec = 10000000;
+float strobe_period_float = 0;
+unsigned long period_microsec = 0;
 const unsigned long non_strobe_period = 1000; //stobe period when not yet in strobe mode - just a light. 1000us == 1000hz
 
-float stobes_per_rot = 13.646;
+float stobes_per_rot = 1;
 
 void rps_counter(); // forward declare
 
 void setup(){
- Serial.begin(9600);
- attachInterrupt(0, rps_counter, FALLING); //interrupt 0 is pin 2
+ Serial.begin(115200);
+ //initial light mode
+ Timer1.initialize(non_strobe_period);  // 10 000 000 us = 10 Hz
+ Timer1.pwm(strobePin, dutyCycle_use);
 
- Timer1.initialize(period_microsec);  // 10 000 000 us = 10 Hz
+ attachInterrupt(0, rps_counter, FALLING); //interrupt 0 is pin 2
 }
 
 void loop(){
 
+  //Check hall effect block timer
+  if(block){
+    detachInterrupt(0);    //Disable interrupt for ignoring multitriggers
+    if(micros()-block_timer >= 100000){
+      block=false;
+      attachInterrupt(0, rps_counter, FALLING); //enable interrupt
+    }
+  }
 
- if (millis() - lastmillis >= 1000){  /*Update every one second, this will be equal to reading frecuency (Hz).*/
+  if(calcnow){
+    detachInterrupt(0);    //Disable interrupt when printing
+    total = 0;
+    for (int x=numreadings-1; x>=1; x--){ //count DOWN though the array, every 70 min this might make one glitch as the timer rolls over
+      period1 = readings[x]-readings[x-1];
+      total = total + period1;
+    }
+    average = total / (numreadings-1); //average period
 
-     detachInterrupt(0);    //Disable interrupt when calculating
-     total = 0;
-     for (int x=numreadings-1; x>=1; x--){ //count DOWN though the array, every 70 min this might make one glitch as the timer rolls over
-       period1 = readings[x]-readings[x-1];
-       total = total + period1;
-     }
+    Serial.println(average);
 
-      average = total / numreadings; //average period
-      if(average>=1000000){ //if spinning at less than 1hz, then stop strobe mode
-        strobing=false;
-      }
-
-     if(strobing){
+    if(average<1000000){ //ie, more than 1hz of rotation.
       strobe_period_float = average / stobes_per_rot ;
-
       period_microsec = (unsigned int) strobe_period_float;
       Timer1.setPeriod(period_microsec);
       Timer1.pwm(strobePin, dutyCycle_use);
     }
-
-    else{ //not strobing
+    else{
       Timer1.setPeriod(non_strobe_period);
       Timer1.pwm(strobePin, dutyCycle_use);
     }
-    lastmillis = millis(); // Update lasmillis
+    calcnow = false;
     attachInterrupt(0, rps_counter, FALLING); //enable interrupt
-    }
+  }
 }
-
 
 void rps_counter(){ /* this code will be executed every time the interrupt 0 (pin2) gets low.*/
   //this is really a bit much code for an ISR, but we only have ~3 rps so it should be ok
   readings[index] = micros();
   index++;
+
+  //Double triggering block
+  block_timer = micros();
+  block=true;
+
   if(index >= numreadings){
    index=0;
-   strobing = true;  //we have enough for an average, start strobing
+   calcnow=true;
   }
-
 }
